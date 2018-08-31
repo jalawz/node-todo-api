6bf603d476c83e1b6b93c49e0242bf0bf7f3cbb6
paulomenezes-sciensa <paulo.roberto@sciensa.com>
2018-08-31 08:51:57 -0300

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -21,8 +21,16 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.status(200).send({todos});
+    }, err => {
+        res.status(400).send(err);
+    });
+});
+
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
 
-module.exports = {app};
+module.exports = { app };
