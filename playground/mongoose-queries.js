fa7396a778630d88147b36f2a6569d1513059e3f
paulomenezes-sciensa <paulo.roberto@sciensa.com>
2018-08-31 09:44:31 -0300

    Add queries playgound file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,39 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// const id = '5b892d87e4c4636e1dd8509d11';
+
+// if (!ObjectID.isValid(id)) {
+//     console.log('ID not valid');
+// }
+
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log('Todos', todos);
+// });
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//     if (!todo) {
+//         return console.log('Id not found');
+//     }
+//     console.log('Todo By Id', todo);
+// }).catch((e) => console.log(e));
+
+User.findById('5b87c7ee29e0356c2ef9a262').then((user) => {
+    if (!user) {
+        return console.log('Unable to find user');
+    }
+    console.log(JSON.stringify(user, undefined, 2));
+}, err => {
+    console.log(err);
+});
